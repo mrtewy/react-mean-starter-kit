673b74e4097e197bbba0d7122df3e8344ceabb2b
Tawanwong <tawanwong@softcontrol.net>
2016-03-02 14:51:26 +0700

    update setting

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
-
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
+const port = 3000;
 const app = express();
+const isEnvironment = process.env.NODE_ENV !== 'production'; 
 
-if (isDeveloping == 'development') {
+// if in development environment
+if (isEnvironment) {
+  console.log("THIS");
   const compiler = webpack(config);
   const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
@@ -32,18 +33,17 @@ if (isDeveloping == 'development') {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'public/index.html')));
     res.end();
   });
-} else {
 
+} else {
   app.use(express.static(__dirname + '/public'));
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
-
 }
 
 app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
     console.log(err);
   }
-  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  console.info('Running server in %s and listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', process.env.NODE_ENV, port, port);
 });
